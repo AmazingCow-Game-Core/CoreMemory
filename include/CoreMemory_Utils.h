7e6b71d7be7ac6b1d19856f27583ef45468e3dc2
n2omatt <n2o.matt@gmail.com>
2016-07-15 16:45:56 -0300

    [Version Update v0.2.1] -- General --     - Update the mainpage.dox and README.md to       use the correct amazing cow links.     - Start using the update_version script.

--- include/CoreMemory_Utils.h
+++ include/CoreMemory_Utils.h
@@ -53,8 +53,11 @@
 //The core version number.
 #define COW_COREMEMORY_VERSION_MAJOR    "0"
 #define COW_COREMEMORY_VERSION_MINOR    "2"
-#define COW_COREMEMORY_VERSION_REVISION "0"
+#define COW_COREMEMORY_VERSION_REVISION "1"
 
-#define COW_COREMEMORY_VERSION "0.2.0"
+#define COW_COREMEMORY_VERSION       \
+    COW_COREMEMORY_VERSION_MAJOR "." \
+    COW_COREMEMORY_VERSION_MINOR "." \
+    COW_COREMEMORY_VERSION_REVISION
 
 #endif // defined(__CoreMemory_include_CoreMemory_Utils_h__) //
